11d135e9f7e06150e5c4ea30062f08dd0b54eec3
UserKazun <kazu_usj_lo@ezweb.ne.jp>
2019-06-06 14:57:46 +0900

    [update]htmlフォルダの存在先を変更

--- src/assets/js/apiRequest.js
+++ src/assets/js/apiRequest.js
@@ -2,8 +2,38 @@ function get_need_response(method, endpoint, key, parameters, callback) {
     // XMLHttpRequestコンストラクタを作成
     var request = new XMLHttpRequest();
 
+    request.onreadystatechange = function () {
+        switch(request.readyState) {
+            case 0:
+                // 未初期状態
+                console.log("リクエストは初期化されていません。");
+                break;
+            case 1:
+                // データ送信中
+                console.log("データを送信中です。");
+                break;
+            case 2:
+                // 応答待ち
+                console.log("応答待ちです。");
+                break;
+                // データ受信中
+            case 3:
+                console.log("データ受信中です。" + request.responseText.length + "bytes.");
+                break;
+            case 4:
+                // データ受信完了
+                if (request.status == 200 || request.status == 304) {
+                    var response_text = request.responseXML;
+                    console.log("完了" + response_text);
+                } else {
+                    console.log("リクエスト失敗です。 HttpStatus: " + request.statusText);
+                }
+                break;
+        }
+    };
+
     // Httpリクエストに必要な情報
-    request.open(method, endpoint);
+    request.open(method, endpoint, true);
     request.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
     request.responseType = 'json';
 

5ce2e951b59fde5d56b3cfa12753ff6a7f8c6bf6
UserKazun <kazu_usj_lo@ezweb.ne.jp>
2019-06-06 16:19:38 +0900

    [update]apiRequest.jsの更新

--- src/assets/js/apiRequest.js
+++ src/assets/js/apiRequest.js
@@ -1,32 +1,40 @@
-function response(method, endpoint, key, parameters, callback) {
+function apiResponse(method, endpoint, key, parameters, callback) {
     // XMLHttpRequestコンストラクタを作成
     var request = new XMLHttpRequest();
 
+    // ハンドラーの登録
     request.onreadystatechange = function () {
         switch(request.readyState) {
             case 0:
                 // 未初期状態
-                console.log("リクエストは初期化されていません。");
+                console.log("Not initialized ... ");
                 break;
             case 1:
                 // データ送信中
-                console.log("データを送信中です。");
+                console.log("Sending data ... ");
                 break;
             case 2:
                 // 応答待ち
-                console.log("応答待ちです。");
+                console.log("Waiting for response ... ");
                 break;
                 // データ受信中
             case 3:
-                console.log("データ受信中です。" + request.responseText.length + "bytes.");
+                console.log("Receiving of data ... ");
                 break;
             case 4:
                 // データ受信完了
-                if (request.status == 200 || request.status == 304) {
-                    var response_text = request.responseXML;
-                    console.log("完了" + response_text);
+                if (request.status == 200 || request.status === 304) {
+                    var response_data = this.response;
+                    if (key === '') {
+                        console.log("Complete!!\n" + response_data);
+                        callback(response_data);
+                    } else {
+                        console.log("Complete!!\n" + response_data[key]);
+                        // コールバック
+                        callback(response_data[key]);
+                    }
                 } else {
-                    console.log("リクエスト失敗です。 HttpStatus: " + request.statusText);
+                    console.log( 'Failed. HttpStatus: ' + request.statusText)
                 }
                 break;
         }
@@ -36,25 +44,6 @@ function response(method, endpoint, key, parameters, callback) {
     request.open(method, endpoint, true);
     request.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
     request.responseType = 'json';
-
-    // 実行
-    request.onload = function () {
-        var response_from_database = this.response;
-        // ステータスコードによって処理を変更
-        if (request.status === 401) {
-            alert("リクエストデータが見つかりません");
-            console.log(response_from_database);
-        } else if (key === '') {
-            console.log(response_from_database);
-            // コールバック
-            callback(response_from_database);
-        } else {
-            console.log(response_from_database[key]);
-            // コールバック
-            callback(response_from_database[key]);
-        }
-
-    };
 
     // Httpリクエストを送信(引数指定で特定の情報のみを送信)
     request.send(parameters);

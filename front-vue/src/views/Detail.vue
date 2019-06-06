b1c2f14b85ad629acc2851499beb020aee5cbe60
YudaiShiratori <toritorikebabu1023x@gmail.com>
2019-06-06 15:27:09 +0900

    vue create

---
+++ front-vue/src/views/Detail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="detail">
+    <p>aaaaa</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+
+export default class Detail extends Vue {}
+</script>
+
